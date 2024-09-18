f4cd3a9c02ec05f83109954496b37c5895a9acfc
KorrinaG <korrinamary@hotmail.com>
2024-09-18 15:00:16 -0700

    removed hashes

--- Turtle code.R
+++ Turtle code.R
@@ -1,8 +1,6 @@
 #See tutorial https://www.datacamp.com/tutorial/r-data-import-tutorial
 #### set working directory ####
 setwd("/Users/korri/Desktop/R files") 
-#### 
-####
 
 ## Install packages
 BiocManager::install("microbiome")
